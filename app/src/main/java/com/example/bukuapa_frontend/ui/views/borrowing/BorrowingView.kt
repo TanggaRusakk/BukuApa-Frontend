f41daaec5f72a9098daa6a26eaf1a79ce77f48ba
Angga Dhammika Jaya <anggadj94@gmail.com>
2026-06-04 15:27:08 +0700

    Fix Error Merge

--- app/src/main/java/com/example/bukuapa_frontend/ui/views/borrowing/BorrowingView.kt
+++ app/src/main/java/com/example/bukuapa_frontend/ui/views/borrowing/BorrowingView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Book
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -14,13 +15,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImage
 import com.example.bukuapa_frontend.data.models.Loan
 import com.example.bukuapa_frontend.ui.viewmodels.borrowing.BorrowingViewModel
 import com.example.bukuapa_frontend.ui.views.components.TopNavigatorBar
