8a9176b96f2eb5e923bb0e9411a850584b6d14c1
Angga Dhammika Jaya <anggadj94@gmail.com>
2026-06-11 21:24:02 +0700

    Fix error

--- app/src/main/java/com/example/bukuapa_frontend/data/api/ApiResponse.kt
+++ app/src/main/java/com/example/bukuapa_frontend/data/api/ApiResponse.kt
@@ -1,16 +1,3 @@
 package com.example.bukuapa_frontend.data.api
 
-import com.example.bukuapa_frontend.data.models.Book
-import com.example.bukuapa_frontend.data.models.CanReviewResponse
-import com.example.bukuapa_frontend.data.models.Category
-import com.example.bukuapa_frontend.data.models.CreateReviewRequest
-import com.example.bukuapa_frontend.data.models.Loan
-import com.example.bukuapa_frontend.data.models.Review
-import com.example.bukuapa_frontend.data.models.ReviewListResponse
-import com.example.bukuapa_frontend.data.models.UpdateReviewRequest
-import com.example.bukuapa_frontend.data.models.User
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.*
-
 data class ApiResponse<T>(val data: T, val message: String? = null)
